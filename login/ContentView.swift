17e11dba58eb758c5cc5299773bfa97a55f88d1b
Abdulrahman Alaa <46077105+youssefes@users.noreply.github.com>
2023-02-15 13:16:05 +0200

    Initial commit

---
+++ login/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by Abu Anwar MD Abdullah on 23/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+    var body: some View {
+        VStack {
+            WelcomeScreen()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
